29efb3225a1b8dc6331b7aa58d810283f5f69717
hhenrichsen <hhenrichsen@users.noreply.github.com>
2021-09-23 09:32:24 +0000

    deploy: 579a27a0b06c22157ebdbc6b6b666540e43283dd

---
+++ js/codeblock.js
@@ -0,0 +1,63 @@
+(() => {
+  // <stdin>
+  (function() {
+    "use strict";
+    if (!document.queryCommandSupported("copy")) {
+      return;
+    }
+    function flashCopyMessage(el, msg) {
+      var original = el.innerHTML;
+      el.textContent = msg;
+      setTimeout(function() {
+        el.innerHTML = original;
+      }, 1e3);
+    }
+    function selectText(node) {
+      var selection = window.getSelection();
+      var range = document.createRange();
+      range.selectNodeContents(node);
+      selection.removeAllRanges();
+      selection.addRange(range);
+      return selection;
+    }
+    function addCopyButton(containerEl) {
+      var copyBtn = document.createElement("button");
+      copyBtn.className = "highlight-copy-btn";
+      var icon = document.createElement("i");
+      icon.setAttribute("data-feather", "clipboard");
+      copyBtn.appendChild(icon);
+      var elements = containerEl.querySelectorAll("pre");
+      console.log(elements);
+      var codeEl;
+      if (elements.length > 1) {
+        codeEl = elements[1].parentElement;
+      } else {
+        codeEl = elements[0];
+      }
+      console.log(codeEl);
+      new SimpleBar(codeEl, {
+        autoHide: true
+      });
+      copyBtn.addEventListener("click", function() {
+        try {
+          var selection = selectText(codeEl.firstElementChild);
+          document.execCommand("copy");
+          selection.removeAllRanges();
+          flashCopyMessage(copyBtn, "Copied!");
+        } catch (e) {
+          console && console.log(e);
+          flashCopyMessage(copyBtn, "Failed :'(");
+        }
+      });
+      containerEl.appendChild(copyBtn);
+    }
+    var highlightBlocks = document.getElementsByClassName("highlight");
+    console.log(highlightBlocks);
+    var blockArr = Array.prototype.slice.call(highlightBlocks);
+    console.log(blockArr);
+    blockArr.forEach((el) => {
+      addCopyButton(el);
+    });
+    feather.replace();
+  })();
+})();
---
+++ js/header.js
@@ -0,0 +1,29 @@
+(() => {
+  // <stdin>
+  (function() {
+    const article = document.getElementById("article");
+    if (!article) {
+      return;
+    }
+    const headings = article.querySelectorAll("h2, h3, h4, h5, h6");
+    headings.forEach((heading) => {
+      const div = document.createElement("div");
+      div.classList.add("heading");
+      const slug = heading.innerText.toLowerCase().replace(/ /g, "-").replace(/[^\w-]/g, "");
+      const link = document.createElement("a");
+      link.href = `#${slug}`;
+      const icon = document.createElement("i");
+      icon.setAttribute("data-feather", "link");
+      link.appendChild(icon);
+      heading.parentElement.replaceChild(div, heading);
+      div.appendChild(heading);
+      div.appendChild(link);
+      link.addEventListener("click", (event) => {
+        const baseLocation = window.location.href.slice(0, window.location.href.indexOf("#"));
+        event.preventDefault();
+        navigator.clipboard.writeText(`${baseLocation}#${slug}`);
+      });
+    });
+    feather.replace();
+  })();
+})();
