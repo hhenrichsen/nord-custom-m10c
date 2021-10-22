5b3c74c7591c9aa24d78f8ae63f0f10ed69b7c2a
hhenrichsen <hhenrichsen@users.noreply.github.com>
2021-10-22 07:00:52 +0000

    deploy: 7f0efa89d2a555a60e53cc1b3fb0e45633e7a5f7

--- js/bars.js
+++ js/bars.js
@@ -2,7 +2,7 @@
   // <stdin>
   document.querySelectorAll("table").forEach((table) => {
     const element = document.createElement("div");
-    table.parentElement.appendChild(element);
+    table.parentElement.replaceChild(element, table);
     element.appendChild(table);
     new SimpleBar(element, { autoHide: true });
   });
