1f4355e8d6e853941fbd2eebe38429e05afd546b
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2021-10-21 23:03:56 -0600

    Multiple styling tweaks. Add light mode toggle.

---
+++ assets/js/bars.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('table').forEach(table => {
+    const element = document.createElement('div');
+    table.parentElement.appendChild(element);
+    element.appendChild(table);
+    new SimpleBar(element, { autoHide: true })
+
+});
---
+++ assets/js/init.js
@@ -0,0 +1,38 @@
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', e => {
+    const dark = e.matches
+    if (dark) {
+      document.documentElement.classList.add('light-mode')
+    } else {
+      document.documentElement.classList.remove('light-mode')
+    }
+  })
+
+function init () {
+  const override = localStorage.getItem('lm') == 'true'
+  const dark =
+    override != null
+      ? override
+      : window.matchMedia('(prefers-color-scheme: dark)')
+  if (dark) {
+    document.documentElement.classList.add('light-mode')
+  } else {
+    document.documentElement.classList.remove('light-mode')
+  }
+}
+
+function toggleDarkMode () {
+  const classList = document.documentElement.classList
+  if (classList.contains('light-mode')) {
+    localStorage.setItem('lm', 'false')
+    classList.remove('light-mode')
+  } else {
+    localStorage.setItem('lm', 'true')
+    classList.add('light-mode')
+  }
+}
+
+document.querySelector('#dark-toggle').addEventListener('click', toggleDarkMode)
+
+init()
