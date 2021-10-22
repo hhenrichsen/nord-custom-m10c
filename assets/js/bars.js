7f0efa89d2a555a60e53cc1b3fb0e45633e7a5f7
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2021-10-22 01:00:33 -0600

    Fix tables being brought to bottom.

--- assets/js/bars.js
+++ assets/js/bars.js
@@ -1,7 +1,6 @@
 document.querySelectorAll('table').forEach(table => {
-    const element = document.createElement('div');
-    table.parentElement.appendChild(element);
-    element.appendChild(table);
-    new SimpleBar(element, { autoHide: true })
-
-});
+  const element = document.createElement('div')
+  table.parentElement.replaceChild(element, table)
+  element.appendChild(table)
+  new SimpleBar(element, { autoHide: true })
+})
